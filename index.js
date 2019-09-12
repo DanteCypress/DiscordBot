6c94dbce4cc36e388fc2050a432d09b232c7e7ac
Dante Moore <dante@Dante-Cypress.attlocal.net>
2019-09-12 16:53:08 -0700

    bot online

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
 
-const token = "b_HwbmbScPftQbozsmhk0u4FvMQK4tQ9";
+const token = "NjIxODUxODI3NTQ5MjQxMzg2.XXrZ4g.TRrUqnd8jdkKF3O3bdth06PwMS4";
+bot.on("ready", () => {
+  console.log("bot online");
+});
+bot.login(token);
